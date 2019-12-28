a5b493413b837f05562d04e944665964d06034eb
Alex Velasquez <jtinnesco@gmail.com>
2019-12-28 02:45:09 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,11 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+
+  def change 
+    create_tabele :songs do |t|
+      t.string :name 
+      t.integer :artist_id 
+      t.integer :genre_id 
+    end
+  end
+  
 end
